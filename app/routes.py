20c91594a15eec1baf1d92d5f464b74b0c256438
kaleb loar <loark@ainfosec.com>
2021-06-06 19:38:36 -0400

    added plotly graph for simulation viewing fixed bad collection dropping

--- app/routes.py
+++ app/routes.py
@@ -190,13 +190,27 @@ def simulate():
 @flask_app.route('/data', methods=['GET'])
 def data():
     title = 'Data Viewer'
-    return render_template('data.html', title=title)
+    graph_data = []
+    for account in accounts:
+        acc_docs = NoSQL_Account.objects(account_name=account)
+        trace = {
+            'type': "scatter",
+            'mode': "lines",
+            'name': account,
+            'x': [f'{doc.year}-{doc.month}-{doc.day}' for doc in acc_docs],
+            'y': [doc.value for doc in acc_docs],
+        }
+        graph_data.append(trace)
+    layout = {'title': 'Simulation X'}
+    return render_template('data.html', title=title,
+                           graph_data=graph_data, layout=layout)
 
 @flask_app.route('/drop', methods=['GET', 'POST'])
 def drop():
     title = 'Drop Form'
     form = DropCollectionsForm()
     if form.validate_on_submit(): # POST
+        NoSQL_Account.drop_collection()
         db.drop_collection('finprojx_app')
         flash('Collection Dropped')
     return render_template('drop.html', title=title, form=form)
